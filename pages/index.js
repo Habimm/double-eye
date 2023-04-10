b31ebe9dbcf98d096473382659f4123c331ff190
Dimitri <habimm@pm.me>
2023-04-10 22:46:49 +0200

    code fuer doubleeye

--- pages/index.js
+++ pages/index.js
@@ -6,33 +6,32 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Pragmatic Timetabling</title>
+        <title>DoubleEye - Compute Cloud</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <div className="container">
           <div className="row">
             <div className="col-12 text-center">
-              <img src="/logo.png" alt="Pragmatic Timetabling Logo" className="img-fluid" style={{ maxWidth: '100%', height: 'auto' }} />
+              <img src="/logo.png" alt="DoubleEye Logo" className="img-fluid" style={{ maxWidth: '90%', height: 'auto' }} />
               <br />
               <br />
             </div>
           </div>
           <div className="row">
             <div className="col-12 col-md-10 offset-md-1">
-              <p>Entdecken Sie die bahnbrechende Stundenplan-Revolution mit unserer innovativen Software! 🚀</p>
-              <p>Präsentation: Pragmatic Timetabling – Die atemberaubende All-in-One-Lösung für die Erstellung von Stundenplänen für Ihre gesamte Schule: von der Oberstufe bis zur Unterstufe!</p>
+              <p>Entdecken Sie den innovativen Marktplatz für gemeinsam genutzte Rechenleistung!</p>
+              <p>Präsentation: DoubleEye - Die leistungsstarke Plattform für den Zugriff auf GPUs und CPUs von der Community, für die Community.</p>
               <ul>
-                <li>🖥️ Web-basiert: Vergessen Sie mühselige Installationen, starten Sie direkt im Browser und erleben Sie die Zukunft der Stundenplanerstellung!</li>
-                <li>👩‍🏫 Lehrer-Management: Organisieren Sie Ihr Lehrpersonal effizient mit individuellen Profilen und einer Liste der unterrichteten Fächer</li>
-                <li>👨‍🎓 Schüler-Management: Behalten Sie den Überblick über die Schüler und deren Wahlfächer, um jedem gerecht zu werden</li>
-                <li>🏫 Raum-Management: Optimieren Sie die Raumverteilung durch die Festlegung von verfügbaren Zeiten und Raum-Eigenschaften (z.B. Computer-Räume)</li>
-                <li>🔮 Automatische Berechnung: Sparen Sie wertvolle Zeit und erstellen Sie alle Stundenpläne (für Schüler, Lehrer, Räume) auf Knopfdruck!</li>
-                <li>✏️ Manuelle Anpassungen: Flexibilität bei Bedarf – wählen Sie Raum, Lehrer oder Schüler aus und passen Sie den Stundenplan individuell an</li>
-                <li>📤 Export: Behalten Sie den Überblick und exportieren Sie Stundenpläne ganz einfach in Excel oder PDF</li>
+                <li>🌐 Online-Plattform: Keine Installation erforderlich, greifen Sie einfach über Ihren Browser auf die Ressourcen zu.</li>
+                <li>🖥️ GPU- und CPU-Management: Finden und bieten Sie GPUs und CPUs nach Ihren individuellen Anforderungen.</li>
+                <li>💡 Vielfältige Anwendungsfälle: Ideal für Maschinelles Lernen, Softwaretests, App-Entwicklung und vieles mehr.</li>
+                <li>🔧 Maßgeschneiderte Lösungen: Wählen Sie aus einer Vielzahl von Konfigurationen, um Ihre spezifischen Anforderungen zu erfüllen.</li>
+                <li>🌱 Nachhaltig: Nutzen Sie ungenutzte Ressourcen und tragen Sie zu einer umweltfreundlichen Informatik bei.</li>
+                <li>👥 Peer-Support: Profitieren Sie von einer Community aus gleichgesinnten Nutzern, die Ihre Interessen und Herausforderungen teilen.</li>
               </ul>
-              <p>Neugierig geworden? Unterstützen Sie die Entwicklung von Pragmatic Timetabling, indem Sie Ihre E-Mail-Adresse hinterlassen! Wir halten Sie über alle Neuigkeiten auf dem Laufenden und informieren Sie, sobald die Software verfügbar ist.</p>
-              <p>Werden Sie Teil der Pragmatic Timetabling Revolution! 🌐 Geben Sie Ihre E-Mail-Adresse in das unten stehende Feld ein und schicken Sie das Formular ab. Freuen Sie sich darauf, die Stundenplanerstellung für immer zu verändern!</p>
+              <p>Interessiert? Unterstützen Sie DoubleEye, indem Sie Ihre E-Mail-Adresse hinterlassen! Wir halten Sie über alle Neuigkeiten auf dem Laufenden und informieren Sie, sobald neue Ressourcen verfügbar sind.</p>
+              <p>Werden Sie Teil der DoubleEye Bewegung! 🌐 Geben Sie Ihre E-Mail-Adresse in das unten stehende Feld ein und schicken Sie das Formular ab. Freuen Sie sich darauf, die Zukunft der gemeinsam genutzten Rechenleistung zu gestalten!</p>
             </div>
           </div>
           <div className="row">
@@ -46,6 +45,10 @@ export default function Home() {
               <button type="submit" className="btn btn-primary">Informiert mich!</button>
             </div>
             <style jsx>{`
+  .form-control {
+    width: 150%;
+    max-width: 900px;
+  }
         .submit-button-container {
           display: flex;
           justify-content: center;
